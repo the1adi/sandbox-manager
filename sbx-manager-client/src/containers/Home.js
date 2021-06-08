9cb89cc3e0ed81c8df77a164ed2a9a2e12372cf9
Aditya Shahani <aditya.shahani@loreal.com>
2021-06-07 21:50:58 -0400

    Setting Up React Project

---
+++ sbx-manager-client/src/containers/Home.js
@@ -0,0 +1,15 @@
+import React from 'react'
+import './Home.css'
+
+export default function Home() {
+    return (
+        <div className="Home">
+            <div className="lander">
+                <h1>Sandbox Request</h1>
+                <p className="text-muted">
+                    Please submit a sandbox request using the form below
+                </p>
+            </div>
+        </div>
+    )
+}
