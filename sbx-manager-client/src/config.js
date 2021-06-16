cdad91eb77d824bf2436aa56d9c75e54fc817330
Aditya Shahani <aditya.shahani@loreal.com>
2021-06-15 22:42:45 -0400

    setting up amplify

---
+++ sbx-manager-client/src/config.js
@@ -0,0 +1,14 @@
+const config = {
+    apiGateway: {
+        REGION: 'us-east-1',
+        URL: 'https://iwhf105kdi.execute-api.us-east-1.amazonaws.com/dev',
+    },
+    cognito: {
+        REGION: 'us-east-1',
+        USER_POOL_ID: 'us-east-1_E3cuXEzli',
+        APP_CLIENT_ID: '3i8ipvuu1spaj6t8c6gpo1jqmu',
+        IDENTITY_POOL_ID: 'us-east-1:371396c7-580c-43df-b835-fdee445ee9ad',
+    },
+}
+
+export default config
--- sbx-manager-client/src/index.js
+++ sbx-manager-client/src/index.js
@@ -3,7 +3,28 @@ import ReactDOM from 'react-dom'
 import './index.css'
 import App from './App'
 import reportWebVitals from './reportWebVitals'
+import { Amplify } from 'aws-amplify'
+import config from './config'
 import { BrowserRouter as Router } from 'react-router-dom'
+
+Amplify.configure({
+    Auth: {
+        mandatorySignIn: true,
+        region: config.cognito.REGION,
+        userPoolId: config.cognito.USER_POOL_ID,
+        identityPoolId: config.cognito.IDENTITY_POOL_ID,
+        userPoolWebClientId: config.cognito.APP_CLIENT_ID,
+    },
+    API: {
+        endpoints: [
+            {
+                name: 'sandbox',
+                endpoint: config.apiGateway.URL,
+                region: config.apiGateway.REGION,
+            },
+        ],
+    },
+})
 
 ReactDOM.render(
     <React.StrictMode>
