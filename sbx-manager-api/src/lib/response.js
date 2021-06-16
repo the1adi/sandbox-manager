b84e6b645c9d4f395d529e3d9849bee0ec5c9763
Aditya Shahani <aditya.shahani@loreal.com>
2021-06-15 21:45:52 -0400

    Added handling for CORS and IAM Authorizer

--- sbx-manager-api/src/lib/response.js
+++ sbx-manager-api/src/lib/response.js
@@ -2,6 +2,7 @@ module.exports.create = (status, data) => ({
     statusCode: status,
     headers: {
         'Access-Control-Allow-Origin': '*',
+        'Access-Control-Allow-Credentials': true,
     },
     body: JSON.stringify(data),
 })
